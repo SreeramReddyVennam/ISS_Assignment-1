6c5f45e582ac4cdb9aa99909f2d407b0f62bcf45
SreeramReddyVennam <96222089+SreeramReddyVennam@users.noreply.github.com>
2022-04-19 21:32:35 +0530

    Fixed q2

--- q2/q2.sh
+++ q2/q2.sh
@@ -1,1 +1,10 @@
-cat -n $1 | sort -uk2 | sort -nk1 | cut -f2- | awk NF | sed -r "s/(^.*\.)(.*)/\2 once said, \"\1\"/g" | sed -r "s/^..//" > "speech.txt"
+touch speech.txt
+cat $1 | while read line
+do
+    if [[ -z $line ]]
+    then
+        echo >> speech.txt
+        continue
+    fi
+    echo $line | awk -F "~" '{print $2,"once said,","\x22"$1"\x22"}' >> speech.txt
+done
